cbab2d9b4d7cd032e7419a595f6f8ef13308b6e0
Simon Lunay <simonlunay@gmail.com>
2025-08-12 18:00:04 -0400

    open port

--- app.py
+++ app.py
@@ -317,5 +317,6 @@ def predict_nfl():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
